44a72179159d392f15b107bed4588f088037ae7a
GyebnarPeter <gyebnarpetter@gmail.com>
2021-04-29 10:29:00 +0200

    task result

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,16 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public int addDigits(String input) {
+        int result = 0;
+        if(input == null || input.equals("")) {
+            return -1;
+        }
+        String numbers = input.replaceAll("[^0-9.]", "");
+        for (int i = 0; i < numbers.length(); i++) {
+            result += Integer.parseInt(String.valueOf(numbers.charAt(i)));
+        }
+        return result;
+    }
 }
